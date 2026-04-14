bd732a201532e93ab7a00ab502a5610406354fd2
luciobrito <luciobp869@gmail.com>
2026-04-14 11:24:36 -0300

    Frame principal iniciado

---
+++ src/br/edu/fatecguarulhos/caixaeletronico/FrameCaixaEletronico.java
@@ -0,0 +1,64 @@
+package br.edu.fatecguarulhos.caixaeletronico;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
+import javax.swing.BoxLayout;
+import java.awt.FlowLayout;
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import com.jgoodies.forms.layout.FormLayout;
+import com.jgoodies.forms.layout.ColumnSpec;
+import com.jgoodies.forms.layout.RowSpec;
+
+public class FrameCaixaEletronico extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					FrameCaixaEletronico frame = new FrameCaixaEletronico();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public FrameCaixaEletronico() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JButton btnNewButton = new JButton("New button");
+		btnNewButton.setBounds(113, 54, 233, 33);
+		contentPane.add(btnNewButton);
+		
+		JButton button = new JButton("New button");
+		button.setBounds(113, 144, 105, 27);
+		contentPane.add(button);
+		
+		JButton btnNewButton_1 = new JButton("New button");
+		btnNewButton_1.setBounds(113, 99, 233, 33);
+		contentPane.add(btnNewButton_1);
+
+	}
+}
